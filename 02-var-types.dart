6849106e0d5ba79be9b2eec20cc8d69d982ad558
Rodrigo Catalán López <shl@MacBook-Pro-de-Rodrigo.local>
2022-11-04 09:09:29 -0300

    boolean variables and conditions
    Signed-off-by: Rodrigo Catalán López <shl@MacBook-Pro-de-Rodrigo.local>

---
+++ 02-var-types.dart
@@ -0,0 +1,19 @@
+void main()  {
+  
+  // Strings 
+  final String name = 'Tony';
+  final lastName = 'Stark';
+  
+//   name = 'Peter';
+  
+  print('$name $lastName');
+  
+  
+//   Numbers
+  int employee = 10;
+  double salary = 1856.25;
+  
+  print( employee );
+  print( salary );
+  
+}
